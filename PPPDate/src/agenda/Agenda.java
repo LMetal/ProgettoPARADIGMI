6e8c3c495dac4456a8ed155a12996dce014b4e9a
Leonardo Galliera <86923711+LMetal@users.noreply.github.com>
2023-01-08 14:48:21 +0100

    Add files via upload

--- PPPDate/src/agenda/Agenda.java
+++ PPPDate/src/agenda/Agenda.java
@@ -1,15 +1,32 @@
 package agenda;
 
-import java.text.ParseException;
-import java.util.ArrayList;
+import java.util.*;
 
 public class Agenda {
-    private ArrayList<Appuntamento> appuntamenti;
-    private String nomeAgenda;
+    private final ArrayList<Appuntamento> appuntamenti;
+    private final String nomeAgenda;
 
     public Agenda(String nome) {
-        this.nomeAgenda = nome;
-        appuntamenti = new ArrayList<Appuntamento>();
+        this.nomeAgenda = nome; // nome deve essere unico
+        appuntamenti = new ArrayList<>();
+    }
+
+    class IteratoreAgenda implements Iterator<Appuntamento>{
+        int indice;
+
+        IteratoreAgenda(){
+            indice = 0;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return indice < appuntamenti.size();
+        }
+
+        @Override
+        public Appuntamento next() {
+            return appuntamenti.get(indice++);
+        }
     }
 
     public int numApp() {
@@ -20,10 +37,20 @@ public class Agenda {
         return nomeAgenda;
     }
 
-    public void add(String data, String ora, int durata, String nome, String luogo) throws AppuntamentoException, AgendaException {
-        Appuntamento newApp = new Appuntamento(data, ora, durata, nome, luogo);
+
+    private void add(Appuntamento newApp) throws AgendaException {
         validaSovrapposizioni(newApp);
         appuntamenti.add(newApp);
+        appuntamenti.sort(new Comparator<>() {
+            @Override
+            public int compare(Appuntamento a1, Appuntamento a2) {
+                return a1.inzio().compareTo(a2.inzio());
+            }
+        });
+    }
+
+    public void add(String data, String ora, int durata, String nome, String luogo) throws AppuntamentoException, AgendaException {
+        add(new Appuntamento(data, ora, durata, nome, luogo));
     }
 
     private void validaSovrapposizioni(Appuntamento newApp) throws AgendaException {
@@ -36,9 +63,84 @@ public class Agenda {
         // a1         |---------|
         // a2   |-----------|
 
+        if(a1.inzio().before(a2.fine()) && a1.fine().after(a2.fine()) || a1.inzio().compareTo(a2.inzio()) == 0) throw new AgendaException("Presente sovrapposizione");
 
-        if(a1.inzio().compareTo(a2.fine()) <= 0 && a1.fine().compareTo(a2.fine()) >= 0) throw new AgendaException("Presente sovrapposizione");
+        if(a2.inzio().before(a1.fine()) && a2.fine().after(a1.fine()) || a1.fine().compareTo(a2.fine()) == 0) throw new AgendaException("Presente sovrapposizione");
+    }
 
-        if(a2.inzio().compareTo(a1.fine()) <= 0 && a2.fine().compareTo(a1.fine()) >= 0) throw new AgendaException("Presente sovrapposizione");
+    public void modificaData(int i, String newData) throws AppuntamentoException, AgendaException {
+        Appuntamento old = appuntamenti.get(i);
+        Appuntamento newApp = new Appuntamento(newData, old.getOra(), old.getDurata(), old.getNome(), old.getLuogo());
+        appuntamenti.remove(old);
+
+        try {
+            add(newApp);
+        } catch (AgendaException e) {
+            appuntamenti.add(old);
+            throw new AgendaException("Presente sovrapposizione\nAppuntamento non modificato");
+        }
+    }
+
+    public void modificaOra(int i, String newOra) throws AppuntamentoException, AgendaException {
+        Appuntamento old = appuntamenti.get(i);
+        Appuntamento newApp = new Appuntamento(old.getData(), newOra, old.getDurata(), old.getNome(), old.getLuogo());
+        elimina(i);
+
+        try {
+            add(newApp);
+        } catch (AgendaException e) {
+            add(old);
+            throw new AgendaException("Presente sovrapposizione\nAppuntamento non modificato");
+        }
+    }
+
+
+    public void modificaDurata(int numAppuntamento, int newDurata) throws AppuntamentoException, AgendaException {
+        Appuntamento old = appuntamenti.get(numAppuntamento);
+        Appuntamento newApp = new Appuntamento(old.getData(), old.getOra(), newDurata, old.getNome(), old.getLuogo());
+        elimina(numAppuntamento);
+
+        try {
+            add(newApp);
+        } catch (AgendaException e) {
+            add(old);
+            throw new AgendaException("Presente sovrapposizione\nAppuntamento non modificato");
+        }
+    }
+
+    public void modificaNome(int numAppuntamento, String newNome) throws AppuntamentoException, AgendaException {
+        Appuntamento old = appuntamenti.get(numAppuntamento);
+        Appuntamento newApp = new Appuntamento(old.getData(), old.getOra(), old.getDurata(), newNome, old.getLuogo());
+        elimina(numAppuntamento);
+
+        try {
+            add(newApp);
+        } catch (AgendaException e) {
+            add(old);
+            throw new AgendaException("Errore Imprevisto\nAppuntamento non modificato");
+        }
+    }
+
+    public void modificaLuogo(int numAppuntamento, String newLuogo) throws AppuntamentoException, AgendaException {
+        Appuntamento old = appuntamenti.get(numAppuntamento);
+        Appuntamento newApp = new Appuntamento(old.getData(), old.getOra(), old.getDurata(), old.getNome(), newLuogo);
+        elimina(numAppuntamento);
+
+        try {
+            add(newApp);
+        } catch (AgendaException e) {
+            add(old);
+            throw new AgendaException("Errore Imprevisto\nAppuntamento non modificato");
+        }
+    }
+
+    public Iterator<Appuntamento> iterator() {
+        return new IteratoreAgenda();
+    }
+
+    public void elimina(int i) throws AgendaException {
+        if(i >= appuntamenti.size() || i<0) throw new AgendaException("Indice out of bound");
+        appuntamenti.remove(i);
     }
 }
+
---
+++ PPPDate/src/agenda/Interfaccia.java
@@ -0,0 +1,274 @@
+package agenda;
+
+import jbook.util.Input;
+
+import java.util.Iterator;
+
+
+public class Interfaccia {
+    public static void main(String[] argv) {
+        while(true){
+            System.out.println("\nChe operazione si vuole svolgere ? ");
+            System.out.println("1. Visualizzazione Agende");
+            System.out.println("2. Creazione Agenda");
+            System.out.println("3. Eliminazione Agenda");
+            System.out.println("4. Modifica Agenda");
+            System.out.println("0. Exit");
+            try{
+                switch (Input.readInt("\nScelta: ")){
+                    case 1:
+                        visualizzaAgende(true);
+                        break;
+                    case 2:
+                        creaAgenda();
+                        break;
+                    case 3:
+                        eliminaAgenda();
+                        break;
+                    case 4:
+                        operazioniAgenda();
+                        break;
+                    case 0:
+                        return;
+                    default:
+                        System.out.println("ERRORE -> Scelta non valida");
+                }
+            }
+            catch (Exception e) {
+                System.out.println("ERRORE -> Formato scelta non valido\n\n");
+            }
+        }
+    }
+
+    private static Agenda sceltaAgenda(String messaggio){
+        visualizzaAgende(false);
+        System.out.println(messaggio);
+        try{
+            return Agende.getAgenda(Input.readInt());
+        }
+        catch (Exception e) {
+            System.out.println("ERRORE -> Formato scelta non valido\n\n");
+        }
+        return null;
+    }
+
+    private static void eliminaAgenda() {
+        System.out.println(">>> ELIMINAZIONE AGENDA\n\n");
+        Agende.elimina(sceltaAgenda("Che agenda eliminare ? "));
+
+    }
+
+    private static void creaAgenda() {
+        System.out.println(">>> CREAZIONE AGENDA\n\n");
+        System.out.print("Che nome ? ");
+        try {
+            Agende.createAgenda(Input.readString());
+        } catch (AgendeException e) {
+            System.out.println("ERRORE");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void visualizzaAgende(boolean wait) {
+        System.out.println("\nNumero agende: " + Agende.numAgende());
+        System.out.println("\tNOME\tNUM_APPUNTAMENTI");
+        for(int i=0; i<Agende.numAgende(); i++){
+            System.out.println(i + ".\t" + Agende.getAgenda(i).getName() + "\t" + Agende.getAgenda(i).numApp());
+        }
+        if(wait){
+            System.out.println("Premere un INVIO per continuare");
+            Input.readString();
+        }
+    }
+
+    private static void operazioniAgenda() {
+        System.out.println(">>> OPERAZIONI SU AGENDA");
+        metodiAgenda.main(sceltaAgenda("Su che agenda operare ? "));
+    }
+}
+
+class metodiAgenda{
+    public static void main(Agenda agenda){
+        int d;
+        while(true){
+            System.out.println("\nChe operazione si vuole svolgere ? ");
+            System.out.println("1. Visualizzazione Agenda");
+            System.out.println("2. Aggiunta Appuntamento");
+            System.out.println("3. Eliminazione Appuntamento");
+            System.out.println("4. Modifica Appuntamento");
+            System.out.println("0. EXIT");
+
+            try{
+                d = Input.readInt("\nScelta: ");
+                switch (d){
+                    case 0:
+                        return;
+                    case 1:
+                        visualizza(agenda, true);
+                        break;
+                    case 2:
+                        aggiunta(agenda);
+                        break;
+                    case 3:
+                        eliminazione(agenda);
+                        break;
+                    case 4:
+                        modifica(agenda);
+                        break;
+                    default:
+                        System.out.println("ERRORE -> Scelta non valida\n\n");
+                        break;
+                }
+
+            } catch (Exception e) {
+                System.out.println("ERRORE -> Formato scelta non valido\n\n");
+            }
+        }
+    }
+
+    private static void modifica(Agenda a) {
+        System.out.println("\n>>>MODIFICA");
+
+        visualizza(a, false);
+        System.out.println("\nChe appuntamento si vuole modificare ? ");
+        int indice = Input.readInt();
+
+        System.out.println("\nChe cosa si vuole modificare ? ");
+        System.out.println("1. Data Agenda");
+        System.out.println("2. Ora Appuntamento");
+        System.out.println("3. Durata Appuntamento");
+        System.out.println("4. Nome Persona Appuntamento");
+        System.out.println("5. Luogo Appuntamento");
+        System.out.println("0. EXIT");
+
+        visualizza(a, indice);
+
+        System.out.print("SCELTA: ");
+
+        try{
+            switch (Input.readInt()) {
+                case 1 -> {
+                    System.out.print("Inserire nuova data: ");
+                    try {
+                        a.modificaData(indice, Input.readString());
+                    } catch (AppuntamentoException | AgendaException e) {
+                        System.out.println("ERRORE");
+                        System.out.println(e.getMessage());
+                    }
+                }
+
+                case 2 -> {
+                    System.out.print("Inserire nuova ora: ");
+                    try {
+                        a.modificaOra(indice, Input.readString());
+                    } catch (AppuntamentoException | AgendaException e) {
+                        System.out.println("ERRORE");
+                        System.out.println(e.getMessage());
+                    }
+                }
+
+                case 3 -> {
+                    System.out.print("Inserire nuova durata: ");
+                    try {
+                        a.modificaDurata(indice, Input.readInt());
+                    } catch (AppuntamentoException | AgendaException e) {
+                        System.out.println("ERRORE -> Appuntamento non modificato");
+                        System.out.println(e.getMessage());
+                    }
+                }
+
+                case 4 -> {
+                    System.out.print("Inserire nuovo nome della persona: ");
+                    try {
+                        a.modificaNome(indice, Input.readString());
+                    } catch (AppuntamentoException | AgendaException e) {
+                        System.out.println("ERRORE -> Appuntamento non modificato");
+                        System.out.println(e.getMessage());
+                    }
+                }
+
+                case 5 -> {
+                    System.out.print("Inserire nuovo luogo: ");
+                    try {
+                        a.modificaLuogo(indice, Input.readString());
+                    } catch (AppuntamentoException | AgendaException e) {
+                        System.out.println("ERRORE -> Appuntamento non modificato");
+                        System.out.println(e.getMessage());
+                    }
+                }
+
+                case 0 -> {
+                    return;
+                }
+
+                default -> System.out.println("ERRORE\n");
+            }
+        }
+        catch (Exception e){
+            System.out.println("ERRORE -> Formato scelta non valido\n\n");
+        }
+    }
+
+    private static void eliminazione(Agenda a) {
+        System.out.println("\n\n>>>\tRIMOZIONE APPUNTAMENTO\n");
+        visualizza(a, false);
+        System.out.println("Che elemento eliminare ? ");
+        try {
+            a.elimina(Input.readInt());
+        } catch (AgendaException e) {
+            System.out.println("ERRORE -> Appuntamento non aggiunto");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void aggiunta(Agenda a) {
+        /*System.out.println("\n\n>>>\tAGGIUNTA NUOVO APPUNTAMENTO\n");
+        System.out.print("In che data? <gg-mm-aaaa> : ");
+        String data = Input.readString();
+        System.out.print("A che ora? <hh-mm> : ");
+        String ora = Input.readString();
+        System.out.print("Che durata? <min> : ");
+        int durata = Input.readInt();
+        System.out.print("Nome persona? : ");
+        String nome = Input.readString();
+        System.out.print("Dove? : ");
+        String luogo = Input.readString();*/
+
+
+        try {
+            a.add("01-01-2022", "10-20", 120, "Pippo", "luogo");
+        } catch (AppuntamentoException | AgendaException e) {
+            System.out.println("ERRORE -> Appuntamento non aggiunto");
+            System.out.println(e.getMessage());
+        }
+    }
+
+    static void visualizza(Agenda a, boolean wait){
+        Iterator<Appuntamento> iter = a.iterator();
+        int app = 0;
+        System.out.println("APPUNTAMENTI IN AGENDA: " + a.numApp());
+        System.out.println("\tDATA\t\tORA\t\tDURATA\tNOME\tLUOGO");
+        while(iter.hasNext()){
+            System.out.println(app + ".\t" + iter.next());
+            app++;
+        }
+
+        if(wait){
+            System.out.println("Premere un INVIO per continuare");
+            Input.readString();
+        }
+    }
+
+    private static void visualizza(Agenda a, int indice) {
+        Iterator<Appuntamento> iter = a.iterator();
+
+        while (iter.hasNext()){
+            if(indice == 0){
+                System.out.println(iter.next().toString());
+                return;
+            }
+            indice--;
+            iter.next();
+        }
+    }
+}
