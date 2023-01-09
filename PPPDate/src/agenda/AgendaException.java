1d1252093f1b6d3c749faff269ed636d861edaba
Leonardo Galliera <86923711+LMetal@users.noreply.github.com>
2023-01-09 13:52:13 +0100

    Update AgendaException.java

--- PPPDate/src/agenda/AgendaException.java
+++ PPPDate/src/agenda/AgendaException.java
@@ -1,6 +1,6 @@
 package agenda;
 
-public class AgendaException extends Throwable {
+public class AgendaException extends Exception {
     public AgendaException(String messaggio) {
         super(messaggio);
     }
